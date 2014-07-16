10fc557c227d7b917711a0ca79954787db475d53
Parker Johnson <p.johnson@linux.com>
2014-07-16 11:45:48 -0500

    Sourced!

--- xsetcmus.sh
+++ xsetcmus.sh
@@ -1,3 +1,5 @@
+# Code inspired by content found at http://v3gard.com/2011/01/getting-cmus-to-cooperate-with-conky/
+
 if ps -C cmus > /dev/null; then
 	artist=`cmus-remote -Q | grep '^tag artist' | awk '{gsub("tag artist ", "");print}' `
 	title=`cmus-remote -Q  | grep '^tag title' | awk '{gsub("tag title ", "");print}'`

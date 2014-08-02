77a78c9e0d4c457e681061e100d14ad90299c1ee
Parker Johnson <p.johnson@linux.com>
2014-08-02 16:01:48 -0500

    Added catch for artistsort value in MP3's.

--- xsetcmus.sh
+++ xsetcmus.sh
@@ -1,5 +1,5 @@
 if ps -C cmus > /dev/null; then
-	artist=`cmus-remote -Q | grep '^tag artist' | awk '{gsub("tag artist ", "");print}' `
+artist=`cmus-remote -Q | grep '^tag artist' | sed '/^tag artistsort/d'  | awk '{gsub("tag artist ", "");print}' `
 	title=`cmus-remote -Q  | grep '^tag title' | awk '{gsub("tag title ", "");print}'`
 	echo "Now Playing: $artist - $title";
 else

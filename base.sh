dc6f05a3a1417ffc645df10b7311379efdf30548
Parker Johnson <p.johnson@linux.com>
2015-01-16 17:28:33 -0600

    Forgot gstreamer.

--- base.sh
+++ base.sh
@@ -1,6 +1,7 @@
 sudo pacman -S ntfs-3g pacakge-query abs dmenu xorg-xinit xorg-xsetroot xorg-server xf86-video-ati \
 	pcmanfm devmon xorg-xrdb feh oxygen-gtk2 oxygen-gtk3 lxappearance oxygen-icons terminus-font \
-	alsa-utils chromium upower ttf-bitstream-vera mixxx tor vidalia cmus git
+	alsa-utils chromium upower ttf-bitstream-vera mixxx tor vidalia cmus git gstreamer0.10-base-plugins \
+	gstreamer0.10-ugly-plugins gstreamer0.10-good-plugins gstreamer0.10-bad-plugins
 mkdir Builds
 cd Builds
 git clone http://github.com/ronno/playon.sh

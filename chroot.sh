271fc2be0d134104574d683812a81cb6e54c1739
Parker Johnson <p.johnson@linux.com>
2015-01-25 14:03:56 -0600

    Fixed typo.

--- chroot.sh
+++ chroot.sh
@@ -2,7 +2,7 @@ echo theforest > /etc/hostname
 ln -sf /usr/share/zoneinfo/US/Central /etc/localtime
 vim /etc/locale.gen
 locale-gen
-mkinitpcio -p linux
+mkinitcpio -p linux
 systemctl enable wicd.service
 passwd
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub --recheck --debug
